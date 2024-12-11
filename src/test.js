b44e2abc76ca1e63e9bf4dcd94bb4cf97b16c613
Gakyeong <bae21006@byui.edu>
2024-12-12 01:38:18 +0900

    add newword button

--- src/test.js
+++ src/test.js
@@ -12,17 +12,6 @@ function getLocalStorageItem(key) {
 const tasks = getLocalStorageItem("tasks");
 console.log(tasks);
 
-function getRandomTasks(tasks) {
-  function randomN(num) {
-    return Math.floor(Math.random() * num);
-  }
-  const listLength = tasks.length;
-  const randomNum = randomN(listLength);
-  return tasks[randomNum];
-}
-const randomTask = getRandomTasks(tasks);
-console.log(randomTask);
-
 function taskTemplate(task) {
   return `
             <section class="flashcard">
@@ -45,18 +34,29 @@ function taskTemplate(task) {
         </section>`;
 }
 
-function renderTasks(randomObj) {
-  // get the list element from the DOM
-  const listElement = document.querySelector(".thirdpage");
-  listElement.innerHTML = "";
-  listElement.innerHTML = taskTemplate(randomObj);
-}
-renderTasks(randomTask);
-
 function toggleFlipped(event) {
   event.currentTarget.classList.toggle("flipped");
 }
 
-document.querySelector(".flashcard").addEventListener("click", toggleFlipped);
-document.querySelector(".hint1").addEventListener("click", toggleFlipped);
-document.querySelector(".hint2").addEventListener("click", toggleFlipped);
+function renderTasks() {
+  function getRandomTasks(tasks) {
+    function randomN(num) {
+      return Math.floor(Math.random() * num);
+    }
+    const listLength = tasks.length;
+    const randomNum = randomN(listLength);
+    return tasks[randomNum];
+  }
+  const randomTask = getRandomTasks(tasks);
+  console.log(randomTask);
+
+  const listElement = document.querySelector(".thirdpage");
+  listElement.innerHTML = "";
+  listElement.innerHTML = taskTemplate(randomTask);
+
+  document.querySelector(".flashcard").addEventListener("click", toggleFlipped);
+  document.querySelector(".hint1").addEventListener("click", toggleFlipped);
+  document.querySelector(".hint2").addEventListener("click", toggleFlipped);
+}
+
+document.querySelector("#newword").addEventListener("click", renderTasks);
